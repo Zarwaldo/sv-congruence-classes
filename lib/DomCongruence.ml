594292d678c41c472ffc9189664721ff2530b365
THIOLIERE Martin <Martin.Thioliere@bordeaux-inp.fr>
2023-01-13 20:13:22 +0100

    Implementing functions `print`, `equal`, `leq`, `abs`, `empty`, `add`, `sub`, `mul` in file `lib/DomCongruence.ml`. To be completed and tested.

--- lib/DomCongruence.ml
+++ lib/DomCongruence.ml
@@ -1,45 +1,66 @@
 
 type t =
   | Bot
-  | Top
+  | Val of Z.t * Z.t
 
 open Z
 open Z.Compare
 
 let print fmt =
-    failwith "DomCongruence.print not implemented."
+  function
+  | Bot -> Format.pp_print_string fmt "⊥"
+  | Val (r, m) -> Format.fprintf fmt "%d + %dℤ" (Z.to_int r) (Z.to_int m)
 
 let bot = Bot
-let top = Top
+let top = Val(Z.zero, Z.one)
 
 let equal a b =
-    failwith "DomCongruence.equal not implemented."
+  match (a, b) with
+  | (Bot, Bot) -> true
+  | (Bot, _) -> false
+  | (_, Bot) -> false
+  | (Val(r1, m1), Val(r2, m2)) -> (Z.equal r1 r2) && (Z.equal m1 m2)
 
 let leq a b =
-    failwith "DomCongruence.leq not implemented."
+  match (a, b) with
+  | (Bot, Bot) -> true
+  | (Bot, _) -> false
+  | (_, Bot) -> false
+  | (Val(r1, m1), Val(r2, m2)) -> (Z.equal (Z.rem m2 m1) Z.zero) && (Z.equal (Z.rem m2 (Z.sub r1 r2)) Z.zero)
 
 let glb a b =
-    failwith "DomCongruence.glb not implemented."
+  failwith "DomCongruence.glb not implemented."
 
 let lub a b =
-    failwith "DomCongruence.lub not implemented."
+  failwith "DomCongruence.lub not implemented."
 
 let abs x =
-    failwith "DomCongruence.abs not implemented."
+  Val(Z.of_int x, Z.zero)
 
 let empty a =
-    failwith "DomCongruence.empty not implemented."
+  match a with
+  | Bot -> true
+  | Val(_,_) -> false
 
 module Op =
 struct
   let add a b =
-    failwith "DomCongruence.add not implemented."
+    match (a, b) with
+    | (Bot, _) -> Bot
+    | (_, Bot) -> Bot
+    | (Val(a, b), Val(u, v)) -> let d = Z.gcd b v in Val(Z.rem (Z.add a u) d, d)
 
   let sub a b =
-    failwith "DomCongruence.sub not implemented."
+    match (a, b) with
+    | (Bot, _) -> Bot
+    | (_, Bot) -> Bot
+    | (Val(a, b), Val(u, v)) -> let d = Z.gcd b v in Val(Z.rem (Z.sub a u) d, d)
 
   let mul a b =
-    failwith "DomCongruence.mul not implemented."
+    match (a, b) with
+    | (Bot, _) -> Bot
+    | (_, Bot) -> Bot
+    | (Val(a, b), Val(u, v)) -> add (Val(Z.mul u v, Z.zero)) (add (Val(Z.zero, Z.mul a v)) (add (Val(Z.zero, Z.mul u b)) (Val(Z.zero, Z.mul b v))))
 
   let div a b =
     failwith "DomCongruence.div not implemented."
