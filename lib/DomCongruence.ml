a1eafd511570d46b8151cb60aeff91c9b431de62
THIOLIERE Martin <Martin.Thioliere@bordeaux-inp.fr>
2023-01-19 17:00:37 +0100

    Correcting many bugs found through the tests.

--- lib/DomCongruence.ml
+++ lib/DomCongruence.ml
@@ -30,14 +30,20 @@ let leq a b =
   | (Bot, Bot)
   | (Bot, _) -> true
   | (_, Bot) -> false
-  | (Val(a, b), Val(u, _)) when (Z.equal b Z.zero) && (Z.equal a u) -> true
-  | (Val(a, b), Val(u, v)) when (Z.equal a u) -> (Z.equal (Z.rem v b) Z.zero)
-  | (Val(a, b), Val(u, v)) when (Z.equal b Z.zero) -> (Z.equal (Z.rem v (Z.sub a u)) Z.zero)
-  | (Val(a, b), Val(u, v)) -> (Z.equal (Z.rem v b) Z.zero) && (Z.equal (Z.rem v (Z.sub a u)) Z.zero)
+  | (Val(a, b), Val(u, v)) when (Z.equal v Z.zero) -> (Z.equal a u) && (Z.equal b Z.zero)
+  | (Val(a, b), Val(u, v)) -> (Z.divisible b v) && (Z.divisible (Z.sub a u) v)
 
 let glb a b =
-    match (a, b) with
-   | (Val(r1, m1), Val(r2, m2)) -> 
+  match (a, b) with
+  | (_, Bot) -> Bot
+  | (Bot, _) -> Bot
+  | (Val(r1, m1), Val(r2, m2)) when (Z.equal m2 Z.zero) && (Z.equal m1 Z.zero) && r1 == r2 -> Val(r1, Z.zero)
+  | (Val(_, m1), Val(_, m2)) when (Z.equal m2 Z.zero) && (Z.equal m1 Z.zero) -> Bot
+  | (Val(r1, m1), Val(r2, m2)) when (Z.equal m1 Z.zero) && (Z.divisible (Z.sub r1 r2) m2) -> Val(r1, Z.zero)
+  | (Val(_, m1), Val(_, _)) when (Z.equal m1 Z.zero) -> Bot
+  | (Val(r1, m1), Val(r2, m2)) when (Z.equal m2 Z.zero) && (Z.divisible (Z.sub r2 r1) m1) -> Val(r2, Z.zero)
+  | (Val(_, _), Val(_, m2)) when (Z.equal m2 Z.zero) -> Bot
+  | (Val(r1, m1), Val(r2, m2)) ->
     let (pgcd, u, _) = Z.gcdext m1 m2 in
     if Z.divisible (Z.sub r2 r1) pgcd then
       let k = Z.div (Z.sub r2 r1) pgcd in
@@ -47,19 +53,30 @@ let glb a b =
       Val(r, m)
     else
       Bot
-   | (Val(_, _), Bot)
-   | (Bot, Val(_, _))
-   | (Bot, Bot) -> Bot
 
 let lub a b =
   match (a, b) with
- | (Val(r1, m1), Val(r2, m2)) ->
-  let m = Z.gcd m1 (Z.gcd m2 (Z.sub r2 r1)) in
-  let r = Z.erem r1 m in
-  Val(r, m)
- | (Val(r1, m1), Bot) -> Val(r1, m1)
- | (Bot, Val(r2, m2)) -> Val(r2, m2)
- | (Bot, Bot) -> Bot
+  | (Bot, Bot) -> Bot
+  | (Val(r1, m1), Bot) -> Val(r1, m1)
+  | (Bot, Val(r2, m2)) -> Val(r2, m2)
+  | (Val(r1, m1), Val(r2, m2)) when (Z.equal m1 Z.zero) && (Z.equal m2 Z.zero) && (Z.equal r1 r2) ->
+    Val(r1, Z.zero)
+  | (Val(r1, m1), Val(r2, m2)) when (Z.equal m1 Z.zero) && (Z.equal m2 Z.zero) ->
+    let m = Z.sub r1 r2 in
+    let r = Z.erem r1 m in
+    Val(r, m)
+  | (Val(r1, m1), Val(r2, m2)) when (Z.equal m2 Z.zero) ->
+    let m = Z.gcd m1 (Z.sub r1 r2) in
+    let r = Z.erem r2 m in
+    Val(r, m)
+  | (Val(r1, m1), Val(r2, m2)) when (Z.equal m1 Z.zero) ->
+    let m = Z.gcd m2 (Z.sub r2 r1) in
+    let r = Z.erem r1 m in
+    Val(r, m)
+  | (Val(r1, m1), Val(r2, m2)) ->
+    let m = Z.gcd m1 (Z.gcd m2 (Z.sub r2 r1)) in
+    let r = Z.erem r1 m in
+    Val(r, m)
 
 let abs x =
   Val(Z.of_int x, Z.zero)
@@ -76,24 +93,24 @@ struct
     | (Bot, _) -> Bot
     | (_, Bot) -> Bot
     | (Val(a, b), Val(u, v)) when (Z.equal b Z.zero) && (Z.equal v Z.zero) -> Val(Z.add a u, Z.zero)
-    | (Val(a, b), Val(u, v)) when (Z.equal b Z.zero) -> Val(Z.rem (Z.add a u) v, v)
-    | (Val(a, b), Val(u, v)) when (Z.equal v Z.zero) -> Val(Z.rem (Z.add a u) b, b)
-    | (Val(a, b), Val(u, v)) -> let d = Z.gcd b v in Val(Z.rem (Z.add a u) d, d)
+    | (Val(a, b), Val(u, v)) when (Z.equal b Z.zero) -> Val(Z.erem (Z.add a u) v, Z.abs v)
+    | (Val(a, b), Val(u, v)) when (Z.equal v Z.zero) -> Val(Z.erem (Z.add a u) b, Z.abs b)
+    | (Val(a, b), Val(u, v)) -> let d = Z.gcd b v in Val(Z.erem (Z.add a u) d, d)
 
   let sub a b =
     match (a, b) with
     | (Bot, _) -> Bot
     | (_, Bot) -> Bot
     | (Val(a, b), Val(u, v)) when (Z.equal b Z.zero) && (Z.equal v Z.zero) -> Val(Z.sub a u, Z.zero)
-    | (Val(a, b), Val(u, v)) when (Z.equal b Z.zero) -> Val(Z.rem (Z.sub a u) v, v)
-    | (Val(a, b), Val(u, v)) when (Z.equal v Z.zero) -> Val(Z.rem (Z.sub a u) b, b)
-    | (Val(a, b), Val(u, v)) -> let d = Z.gcd b v in Val(Z.rem (Z.sub a u) d, d)
+    | (Val(a, b), Val(u, v)) when (Z.equal b Z.zero) -> Val(Z.erem (Z.sub a u) v, Z.abs v)
+    | (Val(a, b), Val(u, v)) when (Z.equal v Z.zero) -> Val(Z.erem (Z.sub a u) b, Z.abs b)
+    | (Val(a, b), Val(u, v)) -> let d = Z.gcd b v in Val(Z.erem (Z.sub a u) d, d)
 
   let mul a b =
     match (a, b) with
     | (Bot, _) -> Bot
     | (_, Bot) -> Bot
-    | (Val(a, b), Val(u, v)) -> add (Val(Z.mul u v, Z.zero)) (add (Val(Z.zero, Z.mul a v)) (add (Val(Z.zero, Z.mul u b)) (Val(Z.zero, Z.mul b v))))
+    | (Val(a, b), Val(u, v)) -> add (Val(Z.mul a u, Z.zero)) (add (Val(Z.zero, Z.mul a v)) (add (Val(Z.zero, Z.mul b u)) (Val(Z.zero, Z.mul b v))))
 
   let div a b =
     match (a, b) with
@@ -101,8 +118,8 @@ struct
     | (_, Bot) -> Bot
     | (Val(_, _), Val(_, v)) when not (Z.equal v Z.zero) -> top
     | (Val(_, _), Val(u, _)) when (Z.equal u Z.zero) -> bot
-    | (Val(a, _), Val(u, _)) when not (Z.equal (Z.rem a u) Z.zero) -> top
-    | (Val(_, b), Val(u, _)) when not (Z.equal (Z.rem b u) Z.zero) -> top
+    | (Val(a, _), Val(u, _)) when not (Z.divisible a u) -> top
+    | (Val(_, b), Val(u, _)) when not (Z.divisible b u) -> top
     | (Val(a, b), Val(u, _)) -> Val(Z.div a u, Z.div b u)
 
   let equality a b c =
@@ -117,12 +134,12 @@ let widen a b =
     | (Bot, Val(r2, m2)) -> Val(r2, m2)
     | (Val(r1, m1), Bot) -> Val(r1, m1)
     | (Bot, Bot) -> Bot
-    | (Val(r1, m1), Val(r2, m2)) -> if m1 > m2 then top else Val(r2, m2)
+    | (Val(_, m1), Val(r2, m2)) -> if m1 > m2 then top else Val(r2, m2)
 
 let narrow a b =
   match (a, b) with
     | (Bot, Val(_, _))
     | (Val(_, _), Bot)
     | (Bot, Bot) -> Bot
+    | (t, Val(r2, m2)) when equal t top -> Val(r2, m2)
     | (Val(r1, m1), Val(_, _)) -> Val(r1, m1)
-    | (top, Val(r2, m2)) -> Val(r2, m2)
